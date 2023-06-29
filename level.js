136f1d64e5107d860aa52a0fd4cd0b4c95773b02
A-Rene <101147769+Alain-Rene@users.noreply.github.com>
2023-06-29 18:25:47 -0400

    Committed initial files

---
+++ level.js
@@ -0,0 +1,52 @@
+export const levelConfigurations = [
+  {
+    // Themes for the memory game that will change every five levels.
+    theme: 'theme1',
+    levels: [
+      { cards: 8, difficulty: 'easy', timeLimit: 60, powerUps: true},
+      { cards: 12, difficulty: 'easy', timeLimit: 80, powerUps: true},
+      { cards: 16, difficulty: 'medium', timeLimit: 100, powerUps: true},
+      { cards: 20, difficulty: 'medium', timeLimit: 120, powerUps: true},
+      { cards: 25, difficulty: 'hard', timeLimit: 140, powerUps: true}
+    ]
+  },
+  {
+    theme: 'theme2',
+    levels: [
+      { cards: 8, difficulty: 'easy', timeLimit: 60, powerUps: true},
+      { cards: 12, difficulty: 'easy', timeLimit: 80, powerUps: true},
+      { cards: 16, difficulty: 'medium', timeLimit: 100, powerUps: true},
+      { cards: 20, difficulty: 'medium', timeLimit: 120, powerUps: true},
+      { cards: 25, difficulty: 'hard', timeLimit: 140, powerUps: true}
+    ]
+  },
+      theme: 'theme3',
+    levels: [
+      { cards: 8, difficulty: 'easy', timeLimit: 60, powerUps: true},
+      { cards: 12, difficulty: 'easy', timeLimit: 80, powerUps: true},
+      { cards: 16, difficulty: 'medium', timeLimit: 100, powerUps: true},
+      { cards: 20, difficulty: 'medium', timeLimit: 120, powerUps: true},
+      { cards: 25, difficulty: 'hard', timeLimit: 140, powerUps: true}
+    ]
+  },
+  {
+    theme: 'theme4',
+    levels: [
+      { cards: 8, difficulty: 'easy', timeLimit: 60, powerUps: true},
+      { cards: 12, difficulty: 'easy', timeLimit: 80, powerUps: true},
+      { cards: 16, difficulty: 'medium', timeLimit: 100, powerUps: true},
+      { cards: 20, difficulty: 'medium', timeLimit: 120, powerUps: true},
+      { cards: 25, difficulty: 'hard', timeLimit: 140, powerUps: true}
+    ]
+  },
+      {
+    theme: 'theme5',
+    levels: [
+      { cards: 8, difficulty: 'easy', timeLimit: 60, powerUps: true},
+      { cards: 12, difficulty: 'easy', timeLimit: 80, powerUps: true},
+      { cards: 16, difficulty: 'medium', timeLimit: 100, powerUps: true},
+      { cards: 20, difficulty: 'medium', timeLimit: 120, powerUps: true},
+      { cards: 25, difficulty: 'hard', timeLimit: 140, powerUps: true}
+    ]
+  },
+]
---
+++ script.js
@@ -0,0 +1,29 @@
+//import { levelConfigurations } from './level.js';
+let flippedCards = [];
+
+// Toggles the flipped state of the card
+function flipCard(event){
+  const clickedCard = event.target;
+  clickedCard.classList.toggle('flipped');
+
+  flippedCards.push(clickedCard);
+
+  if (flippedCards.length === 2){
+    if (flippedCards[0].dataset.card === flippedCards[1].dataset.card){
+      flippedCards = [];
+    } else{
+      setTimeout(() => {
+        flippedCards.forEach(card => card.classList.remove('flipped'));
+        flippedCards = [];
+      }, 1000);
+    }
+  }
+}
+//Selects card elements(*)
+const cards = document.querySelectorAll('.card');
+
+//Event listener will detect the click interactions
+cards.forEach(card => {
+  card.addEventListener('click', flipCard);
+});
+
